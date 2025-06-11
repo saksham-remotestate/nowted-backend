a53f96b72ea69f678c8e50042bf2d8b7d5ca4c09
saksham-remotestate <saksham.bhatia@remotestate.com>
2025-06-11 14:32:54 +0530

    wip

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,25 +1,31 @@
-import { Request, Response, NextFunction } from "express"
-import jwt from "jsonwebtoken"
-import { userData } from "../interface/user.type";
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 
-export const authMiddleware = (req:Request, res:Response, next:NextFunction) => {
-    const authHeader = req.headers['authorization']
-    const token = authHeader?.split(' ')[1];
-    if (!token) {
-        console.log("token missing")
-        return res.send({
-            status: 401,
-            message: "unauthenticated user"
-        });
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader?.split(" ")[1];
+  if (!token) {
+    console.log("token missing");
+    res.send({
+      status: 401,
+      message: "unauthenticated user",
+    });
+    return;
+  }
+
+  jwt.verify(token, process.env.JWT_TOKEN ?? "123", (err, user) => {
+    if (err) {
+      res.send({
+        status: 403,
+        message: "Access denied",
+      });
+      return;
     }
-    jwt.verify(token, process.env.JWT_TOKEN ?? '123', (err, user) => {
-        if (err) {
-          return res.send({
-            status: 403,
-            message: "Access denied",
-          });
-        }
-        req.user = user;
-        next();
-    })
-}
+    console.log(user);
+  });
+  next();
+};
--- src/routes/folderRoutes.ts
+++ src/routes/folderRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { createFolder, deleteFolder, getAllFolders, getFolderById, restoreFolder, updateFolder } from "../controller/folderController";
+import { authMiddleware } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
 router.post("/", createFolder);
-router.get("/", getAllFolders);
+router.get("/", authMiddleware, getAllFolders);
 router.get("/:id", getFolderById);
 router.patch("/:id", updateFolder);
 router.delete("/:id", deleteFolder);
